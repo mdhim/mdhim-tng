129d5eeaa983981dcd05a266bab34c2a094c359d
jsroman <jhr@lanl.gov>
2013-06-11 08:51:41 -0600

    Filled in subroutine code
    All subroutines return a structure with a "->error" field which
    indicate success or error status.

--- src/client.c
+++ src/client.c
@@ -6,16 +6,41 @@
 
 #include "mdhim.h"
 #include "client.h"
-#include "partitioner.h"
+#include "hash.h"
 
 /*
  * Send put to range server
  *
  * @param md main MDHIM struct
  * @param pm pointer to put message to be sent or inserted into the range server's work queue
- * @return MDHIM_SUCCESS or MDHIM_ERROR on error
+ * @return return_message structure with ->error = MDHIM_SUCCESS or MDHIM_ERROR
  */
-int client_put(struct mdhim_t *md, struct mdhim_putm_t *pm) {
+struct mdhim_rm_t *client_put(struct mdhim_t *md, struct mdhim_putm_t *pm) {
+
+	int return_code;
+	struct mdhim_rm_t *rm;
+	
+	rm = malloc(sizeof(struct mdhim_rm_t));
+	rm->error = MDHIM_SUCCESS ;
+
+	return_code = send_message(md, pm->server_rank, pm);
+	// If the send did not succeed then log the error code and return MDHIM_ERROR
+	if (return_code != MDHIM_SUCCESS) {
+	  mlog(MDHIM_CLIENT_CRIT, "MDHIM Rank: %d - Error: %d from server while sending "
+	       "put record request",  md->mdhim_rank, return_code);
+	  rm->error = MDHIM_ERROR;
+	}
+
+	return_code = receive_message(md, pm->server_rank, (void *) rm);
+	// If the receive did not succeed then log the error code and return MDHIM_ERROR
+	if (return_code != MDHIM_SUCCESS) {
+	  mlog(MDHIM_CLIENT_CRIT, "MDHIM Rank: %d - Error: %d from server while receiving "
+	       "put record request",  md->mdhim_rank, return_code);
+	  rm->error = MDHIM_ERROR;
+	}
+
+	// Return any error code if an error is encountered
+	return rm;
 }
 
 /*
@@ -23,10 +48,34 @@ int client_put(struct mdhim_t *md, struct mdhim_putm_t *pm) {
  * 
  * @param md main MDHIM struct
  * @param bpm pointer to bulk put message to be sent or inserted into the range server's work queue
- * @return MDHIM_SUCCESS or MDHIM_ERROR on error
- */
-int client_bput(struct mdhim_t *md, struct mdhim_bputm_t *bpm) {
+ * @return return_message structure with ->error = MDHIM_SUCCESS or MDHIM_ERROR
+*/
+struct mdhim_rm_t *client_bput(struct mdhim_t *md, struct mdhim_bputm_t *bpm) {
 
+	int return_code;
+	struct mdhim_rm_t *rm;
+	
+	rm = malloc(sizeof(struct mdhim_rm_t));
+	rm->error = MDHIM_SUCCESS ;
+
+	return_code = send_message(md, bpm->server_rank, bpm);
+	// If the send did not succeed then log the error code and return MDHIM_ERROR
+	if (return_code != MDHIM_SUCCESS) {
+	  mlog(MDHIM_CLIENT_CRIT, "MDHIM Rank: %d - Error: %d from server while sending "
+	       "bput record request",  md->mdhim_rank, return_code);
+	  rm->error = MDHIM_ERROR;
+	}
+
+	return_code = receive_message(md, bpm->server_rank, (void *) rm);
+	// If the receive did not succeed then log the error code and return MDHIM_ERROR
+	if (return_code != MDHIM_SUCCESS) {
+	  mlog(MDHIM_CLIENT_CRIT, "MDHIM Rank: %d - Error: %d from server while receiving "
+	       "bput record request",  md->mdhim_rank, return_code);
+	  rm->error = MDHIM_ERROR;
+	}
+
+	// Return any error code if an error is encountered
+	return rm;
 }
 
 /*
@@ -34,10 +83,34 @@ int client_bput(struct mdhim_t *md, struct mdhim_bputm_t *bpm) {
  *
  * @param md main MDHIM struct
  * @param gm pointer to get message to be sent or inserted into the range server's work queue
- * @return MDHIM_SUCCESS or MDHIM_ERROR on error
- */
-int client_get(struct mdhim_t *md, struct mdhim_getm_t *gm) {
+ * @return return_message structure with ->error = MDHIM_SUCCESS or MDHIM_ERROR
+*/
+struct mdhim_rm_t *client_get(struct mdhim_t *md, struct mdhim_getm_t *gm, void *buf) {
 
+	int return_code;
+	struct mdhim_rm_t *rm;
+	
+	rm = malloc(sizeof(struct mdhim_rm_t));
+	rm->error = MDHIM_SUCCESS ;
+
+	return_code = send_message(md, gm->server_rank, gm);
+	// If the send did not succeed then log the error code and return MDHIM_ERROR
+	if (return_code != MDHIM_SUCCESS) {
+	  mlog(MDHIM_CLIENT_CRIT, "MDHIM Rank: %d - Error: %d from server while sending "
+	       "get record request",  md->mdhim_rank, return_code);
+	  rm->error = MDHIM_ERROR;
+	}
+
+	return_code = receive_message(md, gm->server_rank, buf);
+	// If the receive did not succeed then log the error code and return MDHIM_ERROR
+	if (return_code != MDHIM_SUCCESS) {
+	  mlog(MDHIM_CLIENT_CRIT, "MDHIM Rank: %d - Error: %d from server while receiving "
+	       "get record request",  md->mdhim_rank, return_code);
+	  rm->error = MDHIM_ERROR;
+	}
+
+	// Return any error code if an error is encountered
+	return rm;
 }
 
 /*
@@ -45,18 +118,69 @@ int client_get(struct mdhim_t *md, struct mdhim_getm_t *gm) {
  *
  * @param md main MDHIM struct
  * @param bgm pointer to get message to be sent or inserted into the range server's work queue
- * @return MDHIM_SUCCESS or MDHIM_ERROR on error
+ * @return return_message structure with ->error = MDHIM_SUCCESS or MDHIM_ERROR
  */
-int client_bget(struct mdhim_t *md, struct mdhim_bgetm_t *bgm) {
+struct mdhim_rm_t *client_bget(struct mdhim_t *md, struct mdhim_bgetm_t *bgm, void *buf) {
+
+	int return_code;
+	struct mdhim_rm_t *rm;
+	
+	rm = malloc(sizeof(struct mdhim_rm_t));
+	rm->error = MDHIM_SUCCESS ;
+
+	return_code = send_message(md, bgm->server_rank, bgm);
+	// If the send did not succeed then log the error code and return MDHIM_ERROR
+	if (return_code != MDHIM_SUCCESS) {
+	  mlog(MDHIM_CLIENT_CRIT, "MDHIM Rank: %d - Error: %d from server while sending "
+	       "bget record request",  md->mdhim_rank, return_code);
+	  rm->error = MDHIM_ERROR;
+	}
+
+	return_code = receive_message(md, bgm->server_rank, buf);
+	// If the receive did not succeed then log the error code and return MDHIM_ERROR
+	if (return_code != MDHIM_SUCCESS) {
+	  mlog(MDHIM_CLIENT_CRIT, "MDHIM Rank: %d - Error: %d from server while receiving "
+	       "bget record request",  md->mdhim_rank, return_code);
+	  rm->error = MDHIM_ERROR;
+	}
+
+	// Return any error code if an error is encountered
+	return rm;
 }
 
 /*
  * Send delete to range server
  *
- * @return MDHIM_SUCCESS or MDHIM_ERROR on error
+ * @param md main MDHIM struct
+ * @param bgm pointer to get message to be sent or inserted into the range server's work queue
+ * @return return_message structure with ->error = MDHIM_SUCCESS or MDHIM_ERROR
  */
-int client_delete(struct mdhim_t *md, struct mdhim_delm_t *dm) {
+struct mdhim_rm_t *client_delete(struct mdhim_t *md, struct mdhim_delm_t *dm) {
 
+	int return_code;
+	struct mdhim_rm_t *rm;
+	
+	rm = malloc(sizeof(struct mdhim_rm_t));
+	rm->error = MDHIM_SUCCESS ;
+
+	return_code = send_message(md, dm->server_rank, dm);
+	// If the send did not succeed then log the error code and return MDHIM_ERROR
+	if (return_code != MDHIM_SUCCESS) {
+	  mlog(MDHIM_CLIENT_CRIT, "MDHIM Rank: %d - Error: %d from server while sending "
+	       "delete record request",  md->mdhim_rank, return_code);
+	  rm->error = MDHIM_ERROR;
+	}
+
+	return_code = receive_message(md, dm->server_rank, (void *) rm);
+	// If the receive did not succeed then log the error code and return MDHIM_ERROR
+	if (return_code != MDHIM_SUCCESS) {
+	  mlog(MDHIM_CLIENT_CRIT, "MDHIM Rank: %d - Error: %d from server while receiving "
+	       "delete record request",  md->mdhim_rank, return_code);
+	  rm->error = MDHIM_ERROR;
+	}
+
+	// Return any error code if an error is encountered
+	return rm;
 }
 
 /*
@@ -64,10 +188,34 @@ int client_delete(struct mdhim_t *md, struct mdhim_delm_t *dm) {
  *
  * @param md main MDHIM struct
  * @param bgm pointer to get message to be sent or inserted into the range server's work queue
- * @return MDHIM_SUCCESS or MDHIM_ERROR on error
+ * @return return_message structure with ->error = MDHIM_SUCCESS or MDHIM_ERROR
  */
-int client_bdelete(struct mdhim_t *md, struct mdhim_bdelm_t *dm) {
+struct mdhim_rm_t *client_bdelete(struct mdhim_t *md, struct mdhim_bdelm_t *bdm) {
 
+	int return_code;
+	struct mdhim_rm_t *rm;
+	
+	rm = malloc(sizeof(struct mdhim_rm_t));
+	rm->error = MDHIM_SUCCESS ;
+
+	return_code = send_message(md, bdm->server_rank, bdm);
+	// If the send did not succeed then log the error code and return MDHIM_ERROR
+	if (return_code != MDHIM_SUCCESS) {
+	  mlog(MDHIM_CLIENT_CRIT, "MDHIM Rank: %d - Error: %d from server while sending "
+	       "bdelete record request",  md->mdhim_rank, return_code);
+	  rm->error = MDHIM_ERROR;
+	}
+
+	return_code = receive_message(md, bdm->server_rank, (void *) rm);
+	// If the receive did not succeed then log the error code and return MDHIM_ERROR
+	if (return_code != MDHIM_SUCCESS) {
+	  mlog(MDHIM_CLIENT_CRIT, "MDHIM Rank: %d - Error: %d from server while receiving "
+	       "bdelete record request",  md->mdhim_rank, return_code);
+	  rm->error = MDHIM_ERROR;
+	}
+
+	// Return any error code if an error is encountered
+	return rm;
 }
 
 /*
--- src/messages.c
+++ src/messages.c
@@ -12,6 +12,17 @@
  */
 int send_message(struct mdhim_t *md, int dest, void *message) {
 
+	MPI_Status status;
+	int return_code;
+
+	// Send a message to the dest (destination) server
+	return_code = MPI_Send(message, 1, MPI_CHAR, dest, RANGESERV_WORK, md->mdhim_comm);
+
+	// If the send did not succeed then return the error code back
+	if ( return_code != MPI_SUCCESS )
+		return MDHIM_ERROR;
+
+	return MDHIM_SUCCESS
 }
 
 /*
@@ -20,8 +31,22 @@ int send_message(struct mdhim_t *md, int dest, void *message) {
  *
  * @param md      main MDHIM struct
  * @param src     source to receive from 
- * @return        message pointer to message received
+ * @message       pointer for message received
+ * @return MDHIM_SUCCESS or MDHIM_ERROR on error
  */
-int *receive_message(struct mdhim_t *md, int src, void *message) {
+int receive_message(struct mdhim_t *md, int src, void *message) {
 
+	int max_size = 1048576; //At most receive 1MB
+	MPI_Status status;
+	int return_code;
+
+	// Receive a message from src (source) server
+	return_code = MPI_Recv(message, max_size, MPI_CHAR, src, RANGESRV_WORK, md->mdhim_comm, 
+		 &status);
+
+	// If the receive did not succed then return the error code back
+	if ( return_code != MPI_SUCCESS )
+		return MDHIM_ERROR;
+
+	return MDHIM_SUCCESS;
 }
