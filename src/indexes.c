5f4d632bb6353b4ad04922930d2ec5e51e36c41f
acaldwell <acaldwell@rrz-fe1.localdomain>
2014-05-16 10:25:04 -0600

    Added support for index names

--- src/indexes.c
+++ src/indexes.c
@@ -499,8 +499,8 @@ uint32_t get_num_range_servers(struct mdhim_t *md, struct index_t *rindex) {
  * @param  md  main MDHIM struct
  * @return     MDHIM_ERROR on error, otherwise the index identifier
  */
-struct index_t *create_local_index(struct mdhim_t *md, int db_type, int key_type) {
-	struct index_t *li;
+struct index_t *create_local_index(struct mdhim_t *md, int db_type, int key_type, char index_name[]) {
+	struct index_t *li, *check;
 	uint32_t rangesrv_num;
 	int ret;
 
@@ -535,6 +535,14 @@ struct index_t *create_local_index(struct mdhim_t *md, int db_type, int key_type
 	li->myinfo.rank = md->mdhim_rank;
 	li->primary_id = md->primary_index->id;
 	li->stats = NULL;
+	// Check to see if the name passed in has already been taken
+	check = NULL;
+	HASH_FIND_STR(md->indexes, index_name, check);
+	if(check) {
+        goto done;
+    }
+    li->name = tolower(index_name);
+	
 
 	//Figure out how many range servers we could have based on the range server factor
 	li->num_rangesrvs = get_num_range_servers(md, li);		
@@ -598,6 +606,12 @@ done:
 		return NULL;
 	}
 
+	if(check) {
+	    mlog(MDHIM_CLIENT_CRIT, "Rank %d - Error creating local index: Name %s, already exists",
+	            md->mdhim_rank, index_name);
+        return NULL;
+    }
+
 	return li;
 }
 
@@ -616,8 +630,8 @@ done:
 
 struct index_t *create_global_index(struct mdhim_t *md, int server_factor, 
 				    int max_recs_per_slice, 
-				    int db_type, int key_type) {
-	struct index_t *gi;
+				    int db_type, int key_type, char index_name[]) {
+	struct index_t *gi, *check;
 	uint32_t rangesrv_num;
 	int ret;
 
@@ -652,6 +666,13 @@ struct index_t *create_global_index(struct mdhim_t *md, int server_factor,
 	gi->myinfo.rank = md->mdhim_rank;
 	gi->primary_id = gi->type == SECONDARY_INDEX ? md->primary_index->id : -1;
 	gi->stats = NULL;
+	// Check to see if the name passed in has already been taken
+	check = NULL;
+	HASH_FIND_STR(md->indexes, index_name, check);
+	if(check) {
+        goto done;
+    }
+    gi->name = gi->id > 0 ? tolower(index_name) : "primary";
 
 	//Figure out how many range servers we could have based on the range server factor
 	gi->num_rangesrvs = get_num_range_servers(md, gi);		
@@ -721,6 +742,12 @@ done:
 	if (!gi) {
 		return NULL;
 	}
+
+	if(check) {
+	    mlog(MDHIM_CLIENT_CRIT, "Rank %d - Error creating global index: Name %s, already exists",
+	            md->mdhim_rank, index_name);
+        return NULL;
+    }
 
 	return gi;
 }
@@ -930,7 +957,7 @@ struct index_t *get_index(struct mdhim_t *md, int index_id) {
  * =====================================================================================
  */
 struct index_t *
-get_index_by_name ( struct mdhim_t *md, char *index_name )
+get_index_by_name ( struct mdhim_t *md, char index_name[] )
 {
     struct index_t *index;
 
